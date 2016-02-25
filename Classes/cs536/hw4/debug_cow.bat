99081d145d7966ff1187f729713b5f28ca30b55a
Zexi Liu <zl54@tux64-13.cs.drexel.edu>
2016-02-24 21:51:54 -0500

    init commit

--- Classes/cs536/hw4/debug_cow.bat
+++
@@ -1,9 +0,0 @@
-hw4 -f bound-cow.smf > cow_default.xpm
-hw4 -f bound-cow.smf -P > cow_parallel.xpm
-hw4 -f bound-cow.smf -x 1.5 > cow_x1.5.xpm
-hw4 -f bound-cow.smf -x 4.75 -y -3.25 -z 3.3 -P > cow_xyzPar.xpm
-hw4 -f bound-cow.smf -X 0.25 -Y -0.15 -Z 0.3 > cow_XYZ.xpm
-hw4 -f bound-cow.smf -X 0.25 -Y -0.15 -Z 0.3 -j 103 -k 143 -o 421 -p 379 > cow_XYZjkop.xpm
-hw4 -f bound-cow.smf -q -1 -r 1.5 -w -2.0 > cow_q_r_w.xpm
-hw4 -f bound-cow.smf -Q 1.5 -R 1 -W .4 > cow_QRW.xpm
-hw4 -f bound-cow.smf -u -1.5 -v -0.9 -U 1.2 -V 0.7 > cow_uvUV.xpm
--- Classes/cs536/hw4/debug_dragon.bat
+++
@@ -1,3 +0,0 @@
-hw4 -f dragon.smf -u -1.5 -v -1 -U 3 -V 3 -x 1 -y 2 -z 3 > dragon.xpm
-hw4 -f dragon.smf -u -1.5 -v -1 -U 3 -V 3 -x 1 -P > dragon_P.xpm
-hw4 -f dragon-50000.smf -u -1.5 -v -1 -U 3 -V 3 -x 1 -y 2 -z 3 > dragon-50000.xpm
